b335ec9ad3dc01ac40ce26ca9fd8ddc442f767ea
Yuta Togashi <92633211+FahriAdison@users.noreply.github.com>
2022-03-07 13:19:22 +0000

    yntkts

--- plugins/sc.js
+++ plugins/sc.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { itsu, usedPrefix: _p }) => {
 prep = itsu.prepareMessageFromContent(m.chat, { orderMessage: { 
 itemCount: 999999999999, status: 1,
-message: `http://github.com/Rlxfly/bot-tzy\n\nDon't forgot give star and forks this repo:)`,
+message: `Oiginal:http://github.com/Rlxfly/bot-tzy\nRecode:https://github.com/FahriAdison/Ran-Bot\n\nDon't forgot give star and forks this repo:)\nDon't Delete The Ori And Recode Github`,
 orderTitle: 'B',
 sellerJid: '0@s.whatsapp.net',
 thumbnail: global.thumb
