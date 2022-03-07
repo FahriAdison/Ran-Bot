e6af0311d3fc7dbd16ade5f56d14783bc92366c9
Yuta Togashi <92633211+FahriAdison@users.noreply.github.com>
2022-03-08 06:23:01 +0700

    Add Join

--- plugins/joinpermanen.js
+++ plugins/joinpermanen.js
@@ -5,15 +5,15 @@ let handler = async (m, { itsu, text, usedPrefix }) => {
     let [_, code] = text.match(linkRegex) || []
     if (!code) throw 'Link Salah'
     let res = await itsu.acceptInvite(code)
-    m.reply(`Berhasil join grup ${res.gid} *BOT AKAN KELUAR DALAM WAKTU 11JAM 25MENIT*`).then(() => {
-        var jumlahHari = 86400000 * 0.3
-        var now = new Date() * 1
+    m.reply(`Berhasil join grup ${res.gid}`).then(() => {
+        var jumlahHari = 86400000 * 999999
+        var now = new Date() * 999999
         if (now < global.DATABASE.data.chats[res.gid].expired) global.DATABASE.data.chats[res.gid].expired += jumlahHari
         else global.DATABASE.data.chats[res.gid].expired = now + jumlahHari
     })
-    await itsu.sendMessage(res.gid, `Halo,\n *${itsu.user.name}* adalah bot whatsapp yang dibangun dengan Nodejs, Saya baru saja bergabung di grup ini diundang oleh wa.me/${m.sender.split`@`[0]}
+    await itsu.sendMessage(res.gid, `Halo,\n *${itsu.user.name}* adalah bot whatsapp yang dibangun dengan Nodejs, Saya baru saja bergabung di grup ini diundang oleh @${m.sender.split`@`[0]}
     
-ketik *#menu* untuk melihat daftar perintah,Dan Maaf Jika Fiturnya Masih Sedikit Karena Sedang Dalam Tahap Pengembangan`, MessageType.text, { contextInfo: { externalAdReply :{
+ketik *#menu* untuk melihat daftar perintah`, MessageType.text, { contextInfo: { externalAdReply :{
        mediaUrl: ' ',
        mediaType: 4,
        title: 'Ran-Bot',
@@ -22,10 +22,10 @@ ketik *#menu* untuk melihat daftar perintah,Dan Maaf Jika Fiturnya Masih Sedikit
 sourceUrl: 'https://wa.me/17608914335?text=Assalamualaikum'
 }}})
 }
-handler.help = ['join <linkgrup>']
-handler.tags = ['main', 'update']
-handler.command = /^join$/i
-
+handler.help = ['joinp <linkgrup>']
+handler.tags = ['owner','premium']
+handler.command = /^joinp$/i
+handler.premium = true
 handler.rowner = true
 
 module.exports = handler
