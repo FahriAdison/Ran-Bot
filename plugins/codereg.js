ea303fd55ca80fa354f4aa515a13e3ecbec82fee
Yuta Togashi <92633211+FahriAdison@users.noreply.github.com>
2022-03-13 05:48:58 +0700

    ya begitulah
    update reg,add unreg,ceksn,codereg

---
+++ plugins/codereg.js
@@ -0,0 +1,51 @@
+let handler = async(m, { itsu, args, usedPrefix }) => {
+
+    if (args.length == 0) return itsu.reply(m.chat, `Harap masukan code transaksi anda..!!`, m)
+    if (args[0] == '070698' || args[0] == '661528' || args[0] == '878588' || args[0] == '775636' || args[0] == '098786' || args[0] == '975485') {
+
+    if (new Date - global.DATABASE.data.users[m.sender].lastcodereg > 889000000000) {
+    if (new Date - global.DATABASE.data.users[m.sender].lastcodereg < 889000000000) throw `Anda sudah mengambill code register, Code register anda sudah kadaluarsa..`
+
+    global.DATABASE.data.users[m.sender].exp += 1000
+    global.DATABASE.data.users[m.sender].money += 500
+    global.DATABASE.data.users[m.sender].limit += 2
+    global.DATABASE.data.users[m.sender].bank += 1000
+    global.DATABASE.data.users[m.sender].pet += 2
+    global.DATABASE.data.users[m.sender].legendary += 1
+    global.DATABASE.data.users[m.sender].diamond += 3
+    global.DATABASE.data.users[m.sender].botol += 10
+    global.DATABASE.data.users[m.sender].kardus += 10
+    global.DATABASE.data.users[m.sender].kaleng += 10
+    global.DATABASE.data.users[m.sender].anggur += 5
+    global.DATABASE.data.users[m.sender].jeruk += 5
+    global.DATABASE.data.users[m.sender].apel += 5
+    global.DATABASE.data.users[m.sender].pisang += 5
+    global.DATABASE.data.users[m.sender].mangga += 5
+    global.DATABASE.data.users[m.sender].lastcodereg = new Date * 1
+    chatnye =`Selamat kamu mendapatkan :\n+1000 Exp ✨\n+500 Money 💹\n+2 Limit 🎫\n+1000 Tabungan 💳\n+2 Pet Crate 📫\n+1 Legendary 🧰\n+3 Diamond 💎\n+10 Kaleng 🥫\n+10 Kardus 📦\n+10 Botol 🍾\n+5 Anggur 🍇\n+5 Jeruk 🍊\n+5 Apel 🍎\n+5 Mangga 🥭\n+5 Pisang 🍌`
+   itsu.reply(m.chat, chatnye, m)
+} else itsu.reply(m.chat, 'Kode anda sudah kadaluarsa, harap transaksi kembali..', m)
+   } else {
+        itsu.reply(m.chat, `*「 KODE TIDAK VALID 」*`, m)
+    }
+}
+
+handler.command = /^(codereg)$/i
+handler.register = true
+handler.limit = true
+
+module.exports = handler
+
+function msToTime(duration) {
+  var milliseconds = parseInt((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+    
+  
+  hours = (hours < 10) ? "0" + hours : hours
+  minutes = (minutes < 10) ? "0" + minutes : minutes
+  seconds = (seconds < 10) ? "0" + seconds : seconds
+
+  return hours + " jam " + minutes + " menit " + seconds + " detik"
+}
--- plugins/register.js
+++ plugins/register.js
@@ -1,37 +1,181 @@
+
+   
 const { createHash } = require('crypto')
+let fetch = require('node-fetch')
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix, command }) {
+	let sn = createHash('md5').update(m.sender).digest('hex')
   let user = global.DATABASE.data.users[m.sender]
-  if (user.registered === true) throw `Kamu sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SERIAL NUMBER>`
-  if (!Reg.test(text)) throw `contoh:\n*${usedPrefix + command} nama.umur*`
+  let namae = itsu.getName(m.sender)
+  if (user.registered === true) return itsu.relayWAMessage(itsu.prepareMessageFromContent(m.chat, {
+        "listMessage": {
+          "title": '*Verify*',
+          "description": '📮 Anda sudah terdaftar! mau daftar ulang?',
+          "footerText": global.botdate,
+          "buttonText": "Ran_2022",
+          "listType": "SINGLE_SELECT",
+          "sections": [
+                            {
+                                "rows": [{
+                                         "title": `LIST MENU`,
+                                         "description": "List Menu Ran_Bot",
+                                         "rowId": ".menu"
+                                    }, {
+                                         "title": `MY OWNER`,
+                                         "description": "List Owner Ran_Bot", 
+                                         "rowId": ".infoowner"
+                                    }, {
+                                         "title": `UNREGISTER`,
+                                         "description": "Daftar Ulang",
+                                         "rowId": '.unreg ' + sn
+                       }],
+                    "title": "Ran_2022♡"
+                  }
+                        ], "contextInfo": 
+                         { "stanzaId": m.key.id,
+                        "participant": m.sender,
+                        "quotedMessage": m.message
+                        }
+                      }
+                     }, {quoted: m, contexInfo: { mentionedJid: [m.sender]}}), {waitForAck: true})
+  if (!Reg.test(text)) return itsu.relayWAMessage(itsu.prepareMessageFromContent(m.chat, {
+        "listMessage": {
+          "title": '*── 「 NOT REGISTERED 」 ──*',
+          "description": `
+📮 Silahkan Pilih umur anda!`,
+          "footerText": global.botdate,
+          "buttonText": "VERIFY",
+          "listType": "SINGLE_SELECT",
+          "sections": [
+                            {
+                                "rows": [{
+                                         "title": '27 Tahun',
+                                         "rowId": '.daftar ' + namae + '.27'
+                                    }, {
+                                         "title": '26 Tahun',
+                                         "rowId": '.daftar ' + namae + '.26'
+                                    }, {
+                                    	"title": '25 Tahun',
+                                         "rowId": '.daftar ' + namae + '.25'
+                                    }, {
+                                    	"title": '24 Tahun',
+                                         "rowId": '.daftar ' + namae + '.24'
+                                    }, {
+                                    	"title": '23 Tahun',
+                                         "rowId": '.daftar ' + namae + '.23'
+                                    }, {
+                                    	"title": '22 Tahun',
+                                         "rowId": '.daftar ' + namae + '.22'
+                                    }, {
+                                    	"title": '21 Tahun',
+                                         "rowId": '.daftar ' + namae + '.21'
+                                    }, {
+                                    	"title": '20 Tahun',
+                                         "rowId": '.daftar ' + namae + '.20'
+                                    }, {
+                                    	"title": '19 Tahun',
+                                         "rowId": '.daftar ' + namae + '.19'
+                                    }, {
+                                    	"title": '18 Tahun',
+                                         "rowId": '.daftar ' + namae + '.18'
+                                    }, {
+                                    	"title": '17 Tahun',
+                                         "rowId": '.daftar ' + namae + '.17'
+                                    }, {
+                                    	"title": '16 Tahun',
+                                         "rowId": '.daftar ' + namae + '.16'
+                                    }, {
+                                    	"title": '15 Tahun',
+                                         "rowId": '.daftar ' + namae + '.15'
+                                    }, {
+                                    	"title": '14 Tahun',
+                                         "rowId": '.daftar ' + namae + '.14'
+                                    }, {
+                                    	"title": '13 Tahun',
+                                         "rowId": '.daftar ' + namae + '.13'
+                                    }, {
+                                    	"title": '12 Tahun',
+                                         "rowId": '.daftar ' + namae + '.12'
+                                    }, {
+                                    	"title": '11 Tahun',
+                                         "rowId": '.daftar ' + namae + '.11'
+                                    }, {
+                                    	"title": '10 Tahun',
+                                         "rowId": '.daftar ' + namae + '.10'
+                                         }, {
+                                    	"title": '9 Tahun',
+                                         "rowId": '.daftar ' + namae + '.9'
+                       }],
+                    "title": "Silahkan Pilih Umur Anda!"
+                  }
+                        ], "contextInfo": 
+                         { "stanzaId": m.key.id,
+                        "participant": m.sender,
+                        "quotedMessage": m.message
+                        }
+                      }
+                     }, {quoted: m, contexInfo: { mentionedJid: [m.sender]}}), {waitForAck: true})
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
   if (!age) throw 'Umur tidak boleh kosong (Angka)'
   age = parseInt(age)
-  if (age > 30) throw 'BAKA!Kamu dah tua'
-  if (age < 5) throw 'Mau diban kak?'
+  if (age > 70) throw 'Umur anda terlalu tua'
+  if (age < 5) throw 'Maaf, minimal 10 tahun'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
-  let sn = createHash('md5').update(m.sender).digest('hex')
-  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.fromMe ? conn.user.jid : m.sender
-  m.reply(`
-━━━━ 「 *Successful Registration* 」━━━━
+  let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
+ 
+  let str = `
+╭───[ *VERIFY* ]───
+│• *Nama* : ${name}
+│• *Umur* : ${age} tahun
+│• *SN* : ${sn}
+│• *ID* : ${pickRandom(['GGA0QH119J1','JPA017P02HA1','PWJROSAIEJ','LAOFOODJQ','PWJFCIFIF','QBBVAODEPZ','QPWEJDNDJD','IQPQBAHSIF','UWUWIWOKPF','PQJDXBXB','AKSJALPQDN','HALAKDILQU','IAIIWIICCIS','KQPSNWO','BSBANSKA','JANSNDKSOS','JDKALDKKAPPPP','KAKALALPCLCLJA','PWWJ017027GA14','19QPDKALLH61J','PWJSJDDUYSYSHSU','1001UQOXJ9JJDY','PQPSODUE107','BBBAKZOOX','00071','WKESOAKWLA','104JALOA9','OSBDOSODO','PWODJDOSKFJSOO','OWJCKEOS','PAJDISO','OSJDOWO','BABAOALKS','PWKW827LA9PA0','PWOEOOOJF','LCKDI2027H393P','IWUEUJEDGWEUEUE','GGGGGGGGHQJU127','HAGFUUXUQP3','P0000009769767T0O87Y5','0TT9I4UO43','P7YYRE8583E6','NZNSJEO','P8Y4W38P0R','P8R6IOP4EU-HG0','0729OWRU','JJJJJJJJAGFIS0W67','000087','BbL016JJQBCSr54OwwW0','BbJdQ0X37ohL016HhqK','BbJdQ0X37ohL016HhqK','GgGddSsssss1039','HAOSOWODJSO','BVjd173k6BEk','AEOKKAKSKKAK20468','BBWUK017KASK','COLIMAJSOAOOAOK1','1238PBLLWO20','KODE123','jajsoskskskOOoO0292','GAGHWPPWSIJ','PWEUPiwpepwp02928','000000723','QPWOEJSJAL','BBAHUSQPEI','HHGGGAGAGODPF','PUS1383PW0292838','HHHB203828LALJK','FFAFUAFAGGAFAFOWVA','PWKDIAPDHZOA','QPWOEUDMZ','103838PW02XLAK','AWOAOAKSPAJDOA','bbbaoduwuslp','kawpajeif','1037pWjs0273BBNjK1','Or4nGB3g04N71nk','Hhhhhhhpwishahhhh','Paosjsh102','Vvvavahsi239hOwpP','PAH103891','RAPEHAL028473','0193910392','103892928','0293829293','02838292','WPPPJSUEUND','01387482923828','gqlI000iwhBYYj0239','DGAOOW','PWOEUDOAO','OSODODOEOEIE','OWRHWOKDIRE','BXKKWCOEDMJCIE','BBDGWOPDDMEO','148920392949202','WOEIOWEOO','19388292929383','MODARSIAHANJINK','BEBEOAODAPJ','LVOWOWIEPADH','AIMALIVETOGOW','JUSTSKOSAO','PW2039LSHDOW','BVOVOVOVUUDEUA307','027492838','01398293','TAPIUPITKAOE103982','019382939','02848292838','OWOWOWOJDJADJLA019','PE023802794949nbK','owjfLjsoKhIOauwo','JKlskeoKJJTRYOJc','IHUOFEtUKPJVD302','HJPgFTIBDTI','029292848','IgwoekdkdomsB','HJOKJYYUlns','INNSAPTAILOEAJD','TAIANJSGSIAOPA','0238829103','02838385842111','9372893739991','BACHEUADL','RC047','BRAINTLUBA0284','DDDDDDDSALDKOE1048','11020393910101010201047GbkL','BrADIopaSLyeYA','JSJDJAKDSKLLLLAPRO','PROGAMERYT123','47492837','923772','BACHELAOF','1038828282','VAKDPOESMAQ0079'])}
+│• *Status* : SUCCES ☑️
+╰──────···─────࿐`.trim()
+itsu.relayWAMessage(itsu.prepareMessageFromContent(m.chat, {
+        "listMessage": {
+          "title": '───────── *Verify* ─────────',
+          "description": str,
+          "footerText": global.botdate,
+          "buttonText": "Ran_2022",
+          "listType": "SINGLE_SELECT",
+          "sections": [
+                            {
+                                "rows": [{
+                                         "title": `LIST MENU`,
+                                         "description": "List Menu Bot",
+                                         "rowId": ".menu"
+                                    }, {
+                                         "title": `CODE REGISTER`,
+                                         "description": "Bonus Pemula", 
+                                         "rowId": `.codereg ${pickRandom(['070698', '661528', '878588', '775636', '098786'])}`
+                                    }, {
+                                         "title": `PROFILE`,
+                                         "description": "Profile Anda!",
+                                         "rowId": '.profile'
+                       }],
+                    "title": "Recode By Papah-Chan"
+                  }
+                        ], "contextInfo": 
+                         { "stanzaId": m.key.id,
+                        "participant": m.sender,
+                        "quotedMessage": m.message
+                        }
+                      }
+                     }, {quoted: fkon, contexInfo: { mentionedJid: [m.sender]}}), {waitForAck: true})
 
-╭─• 〘 INFO 〙
-│➥ Nama: ${name}
-│➥ Umur: ${age} Tahun
-│➥ Hadiah: ${prems.includes(who.split`@`[0]) ? '✅ Silahkan chat owner untuk claim hadiah' : '❌ Bukan User Premium'}
-╰──────•
-
-Ketik ${usedPrefix}sn untuk mendapatkan SERIAL NUMBER\n
-sn digunakan untuk unregister
-`.trim())
 }
-handler.help = ['daftar', 'register'].map(v => v + ' <nama>.<umur>')
-handler.tags = ['xp', 'main']
+handler.help = ['verify', 'daftar']
+handler.tags = ['xp']
 
-handler.command = /^(daftar|reg(ister)?)$/i
+handler.command = /^(verify|daftar|reg(ister)?)$/i
 
 module.exports = handler
+let wm = global.botwm
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
