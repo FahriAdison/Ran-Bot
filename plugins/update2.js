f1bfb15bea692cfd2cb1647ac792d1a1976a44e7
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-03-13 19:26:43 +0700

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'Rlxfly/bot-tzy'
+let repository = 'FahriAdison/Ran-Bot'
 let branch = 'master'
 
 async function handler(m, { text, args }) {
