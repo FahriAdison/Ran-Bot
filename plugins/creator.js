fd2ae1ca68761f24f0b298946c1572d45f68f4d1
Rʟxғʟʏ⁴̅⁰͍⁴̵ <87053694+Rlxfly@users.noreply.github.com>
2022-03-13 16:09:52 +0700

    iyh

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ let handler = function (m) {
   const { MessageType} = require("@adiwajshing/baileys")
 const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
 let fs = require('fs')
-let name = this.contacts[m.sender].notify
+let name = this.contacts[m.sender].notify || this.getName(m.sender);
 let fkon = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
   // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
   
